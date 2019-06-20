d77b211841ab714a56ceb501012e51f0ea9c1202
ss19915 <surajsharma571994@gmail.com>
2019-06-20 17:58:56 +0530

    Added webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -3,41 +3,67 @@ const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
-
+const mode = {
+    PRODUCTION: 'production',
+    DEVELOPMENT: 'development',
+}
 module.exports = (env, argv) => {
+
+    const module = {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: '/node_modules/',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    };
+
+    const plugins = [
+        new CopyPlugin([
+            { from: './src/index.html', to: 'index.html' }
+        ]),
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+        new DotEnv(),
+    ];
+
+    const devServer = {
+        hot: true,
+        port: 3000,
+        historyApiFallback: {
+            index: '/'
+        }
+    }
+
+    let optimization = {};
+
+
+    if(argv.mode === mode.PRODUCTION){
+
+        const productionOptimization = {
+            mangleWasmImports: true,
+            removeAvailableModules: true,
+            removeEmptyChunks: true,
+            removeEmptyChunks: true,
+        };
+
+        optimization = { ...optimization, ...productionOptimization };
+
+    }
     return ({
         entry: './src/index.js',
         output: {
             path: path.resolve(__dirname, 'build'),
             filename: 'bundle.js'
         },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    use: 'babel-loader',
-                    exclude: '/node_modules/',
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader'],
-                },
-            ],
-        },
-        plugins: [
-            new CopyPlugin([
-                { from: './src/index.html', to: 'index.html' }
-            ]),
-            new webpack.HotModuleReplacementPlugin(),
-            new CleanWebpackPlugin(),
-            new DotEnv(),
-        ],
-        devServer: {
-            hot: true,
-            port: 3000,
-            historyApiFallback: {
-                index: '/'
-            }
-        }
+        module,
+        plugins,
+        optimization,
+        devServer
     });
 }
