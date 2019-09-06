d6cc19bda92d709ef1940d6636aa188088d3c269
ss19915 <surajsharma571994@gmail.com>
2019-09-06 20:09:14 +0530

    Added package api(initial commit)

---
+++ packages/api/src/api.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+import middleware from './middleware';
+
+const appPort = process.env.API_PORT || 8081;
+
+const app = express();
+
+app.use([ middleware, routes ]);
+app.listen(appPort, () => {
+    console.log(`Server runnign on Port ${appPort}`);
+});
---
+++ packages/api/src/routes/about.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/about', (req, res, next) => {
+    res.send('Use src/routes to add routes, use src/middleware to add middlewares like auth, and use repository to expose API endpoint methods');
+    next();
+});
+
+export default router;
