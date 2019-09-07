ee5c60c114aba174dea27d8e2cad819017ce7718
ss19915 <surajsharma571994@gmail.com>
2019-09-07 13:50:32 +0530

    added package db

--- packages/api/src/api.js
+++ packages/api/src/api.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import routes from './routes';
 import middleware from './middleware';
+import { connectDb } from '@react-web-app/db';
 
 const appPort = process.env.API_PORT || 8081;
 
@@ -10,3 +11,5 @@ app.use([ middleware, routes ]);
 app.listen(appPort, () => {
     console.log(`Server runnign on Port ${appPort}`);
 });
+
+connectDb();
