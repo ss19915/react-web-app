c7fc45e3bbaa5d5ef27c2a28ef3d2c2a04fa8fa3
ss19915 <surajsharma571994@gmail.com>
2019-09-07 21:05:36 +0530

    removed loader from home

--- packages/ui/src/components/Home/Home.js
+++ packages/ui/src/components/Home/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Paper } from '@react-web-app/theme';
-import Loader from '../CardWithDataSource/Loader';
 
 const Home = () => {
 
     return (
-        <Paper>Home<Loader/></Paper>
+        <Paper>Home</Paper>
     );
 }
 
